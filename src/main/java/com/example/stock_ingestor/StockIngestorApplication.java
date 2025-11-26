4454576341ea7dce864c4ef7b4bcbf871639528b
aadarshchhetry <aadarsh.chhetry00@gmail.com>
2025-11-26 10:37:28 +0530

    initial commit

---
+++ src/main/java/com/example/stock_ingestor/StockIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockIngestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stock_ingestor/StockIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
